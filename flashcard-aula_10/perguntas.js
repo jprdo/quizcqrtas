5da0265b79f6d7e84eedefc287970348ff6de21b
jprdo <00001098755388sp@al.educacao.sp.gov.br>
2024-11-19 11:06:11 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Alimentos',
+    'Qual é a fórmula da maça?',
+    'A=P²L∈'
+)
+
+criaCartao(
+    'Geografia',
+    'O Brail é conhecido pelo que?',
+    'O Brasil é visto pelos estrangeiros como um país de diversidade, alegre, com reunião de pessoas, de festa, por causa do futebol e do Carnaval e onde as pessoas vivem bem.'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos animais de cada espécie Moisés colocou em sua arca: 1, 2 ou 3?',
+    '0! Não foi Moisés quem construiu a arca. Foi Noé'
+)
+
+criaCartao(
+    'Matemática',
+    '230-(220÷2x2÷2) o resultado é 5!',
+    'O resultado é 5! 5x4x3x2x1=120'
+)
